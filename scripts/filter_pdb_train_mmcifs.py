cce725c4a16ec3c18f105fa671fd28c13bbf320b
yoel shoshan <yoels@il.ibm.com>
2024-08-16 02:41:00 -0400

    for some reason the multiprocessing version gets stuck, so used file-locks to be able to spawn multiple separate processes, each running in single process

--- scripts/filter_pdb_train_mmcifs.py
+++ scripts/filter_pdb_train_mmcifs.py
@@ -45,7 +45,7 @@ from Bio.PDB.NeighborSearch import NeighborSearch
 from pdbeccdutils.core import ccd_reader
 from pdbeccdutils.core.ccd_reader import CCDReaderResult
 from tqdm.contrib.concurrent import process_map
-
+from tqdm import tqdm
 from alphafold3_pytorch.common.paper_constants import (
     CRYSTALLOGRAPHY_METHODS,
     LIGAND_EXCLUSION_SET,
@@ -61,7 +61,7 @@ from alphafold3_pytorch.utils.data_utils import (
     is_water,
 )
 from alphafold3_pytorch.utils.utils import exists
-
+from filelock import Timeout, FileLock
 # Constants
 
 FILTER_STRUCTURE_MAX_SECONDS_PER_INPUT = (
@@ -719,7 +719,8 @@ def filter_structure_with_timeout(
     file_id = os.path.splitext(os.path.basename(filepath))[0]
     output_file_dir = os.path.join(output_dir, file_id[1:3])
     output_filepath = os.path.join(output_file_dir, f"{file_id}.cif")
-    os.makedirs(output_file_dir, exist_ok=True)
+    output_filtered_out_filepath = output_filepath+'.FILTERED_OUT'
+    os.makedirs(output_file_dir, exist_ok=True)    
 
     # Filtering of targets
     mmcif_object = mmcif_parsing.parse_mmcif_object(filepath, file_id)
@@ -735,6 +736,7 @@ def filter_structure_with_timeout(
     )
     if not exists(mmcif_object):
         print(f"Skipping target due to prefiltering: {file_id}")
+        _create_empty_file(output_filtered_out_filepath) #mark as filtered out, to avoid reprocessing
         return
     # Filtering of bioassemblies
     # NOTE: Here, we remove waters even though the AlphaFold 3 supplement doesn't mention removing them during filtering
@@ -766,7 +768,13 @@ def filter_structure_with_timeout(
             insert_alphafold_mmcif_metadata=False,
         )
         print(f"Finished filtering structure: {mmcif_object.file_id}")
+    else:
+        _create_empty_file(output_filtered_out_filepath) #mark as filtered out, to avoid reprocessing
 
+
+def _create_empty_file(filepath:str):
+    with open(filepath,'w') as f: 
+        f.write("")
 
 @typecheck
 def filter_structure(args: Tuple[str, str, datetime, datetime, bool]):
@@ -778,6 +786,32 @@ def filter_structure(args: Tuple[str, str, datetime, datetime, bool]):
     file_id = os.path.splitext(os.path.basename(filepath))[0]
     output_file_dir = os.path.join(output_dir, file_id[1:3])
     output_filepath = os.path.join(output_file_dir, f"{file_id}.cif")
+    output_filepath_filtered_out = output_filepath +'.FILTERED_OUT'
+    output_filepath_started_processing = output_filepath +'.STARTED_PROCESSING'
+
+    if os.path.exists(output_filepath_filtered_out):
+        print(f'skipped {file_id} because it did not pass filtering in the past')
+        return
+            
+    if os.path.exists(output_filepath_started_processing):
+        print(f'skipped {file_id} because another process already started processing for it')
+        return
+
+    lock = FileLock(output_filepath_started_processing+'.lock', timeout=0)
+
+    try:
+        with lock.acquire(timeout=0):
+            pass
+    except:
+        #we had a problem getting the lock, skip
+        print('had a problem getting the lock, skipping')
+        return    
+
+    with open(output_filepath_started_processing, 'w') as f:
+        f.write("")
+        f.close()        
+
+    print(f'will start processing {file_id} now ...')
 
     try:
         filter_structure_with_timeout(
@@ -789,6 +823,7 @@ def filter_structure(args: Tuple[str, str, datetime, datetime, bool]):
         )
     except Exception as e:
         print(f"Skipping structure filtering of {filepath} due to: {e}")
+        _create_empty_file(output_filepath_filtered_out) #mark as filtered out, to avoid reprocessing
         if os.path.exists(output_filepath):
             try:
                 os.remove(output_filepath)
@@ -928,9 +963,16 @@ if __name__ == "__main__":
             )
         )
     ]
-    process_map(
-        filter_structure,
-        args_tuples,
-        max_workers=args.no_workers,
-        chunksize=args.chunksize,
-    )
+
+
+    if False:
+        process_map(
+            filter_structure,
+            args_tuples,
+            max_workers=args.no_workers,
+            chunksize=args.chunksize,
+        )
+    else:
+        for curr_args in tqdm(args_tuples, total=len(args_tuples)):
+            print('curr_args=', curr_args)
+            filter_structure(curr_args)
